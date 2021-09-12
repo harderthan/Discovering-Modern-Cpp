78228cc79605f147559613aa54f713a513f486e3
harderthan <kheo1772@gmail.com>
2021-09-12 14:21:25 +0900

    Add exercise3-1

--- Chapter03/exercise3-1.cpp
+++ Chapter03/exercise3-1.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
+#include <sstream>
+#include <string>
 using namespace std;
+
+namespace study{
+    template<typename T>
+    std::string to_string(const T &t)
+    {
+        std::stringstream ss;
+        ss << t;
+        return ss.str();
+    }
+}
 
 int main(int argc, char **argv)
 {
+    // String Type
+    std::string str_type = "Hello, World!";
+    std::cout << study::to_string(str_type) << std::endl;
+
+    // Integer Type
+    int int_type = 33;
+    std::cout << study::to_string(int_type) << std::endl;
+    
+    // Double Type
+    double double_type = 10.0;
+    std::cout << study::to_string(double_type) << std::endl;
+
     return 0;
 }
